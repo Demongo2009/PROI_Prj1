32715d5c8c8ce1d38910b3a867202c2c27041031
Demongo2009 <kuba.galat1@wp.pl>
2019-03-19 18:54:36 +0100

    testing new method of genetating road

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -35,20 +35,20 @@ void Generator::generateRoad(){
   std::uniform_int_distribution<> randomDirection(0,3);
   int startingDirection = randomDirection(gen);
   int tunnelsNumber = tunnelsForSize;
-  switch (startingDirection) {
-    case DOWN:
-      makeSingleRoad(xStarting,yStarting,tunnelsNumber,DOWN);
-      break;
-    case RIGHT:
-      makeSingleRoad(xStarting,yStarting,tunnelsNumber,RIGHT);
-      break;
-    case UP:
-      makeSingleRoad(xStarting,yStarting,tunnelsNumber,UP);
-      break;
-    case LEFT:
-      makeSingleRoad(xStarting,yStarting,tunnelsNumber,LEFT);
-      break;
-  }
+  makeSingleRoad(xStarting,yStarting,tunnelsNumber,startingDirection);
+  // switch (startingDirection) {
+  //   case DOWN:
+  //     break;
+  //   case RIGHT:
+  //     makeSingleRoad(xStarting,yStarting,tunnelsNumber,RIGHT);
+  //     break;
+  //   case UP:
+  //     makeSingleRoad(xStarting,yStarting,tunnelsNumber,UP);
+  //     break;
+  //   case LEFT:
+  //     makeSingleRoad(xStarting,yStarting,tunnelsNumber,LEFT);
+  //     break;
+  // }
 
 }
 
@@ -71,20 +71,20 @@ void Generator::makeSingleRoad(int xNotChecked, int yNotChecked,
     whichDirectionNext = (whichDirectionNext+2)%4;
   }
 
-  switch (whichDirectionNext) {
-    case DOWN:
-      makeSingleRoad(xNotChecked,yNotChecked,tunnelsLeft,DOWN);
-      break;
-    case RIGHT:
-      makeSingleRoad(xNotChecked,yNotChecked,tunnelsLeft,RIGHT);
-      break;
-    case UP:
-      makeSingleRoad(xNotChecked,yNotChecked,tunnelsLeft,UP);
-      break;
-    case LEFT:
-      makeSingleRoad(xNotChecked,yNotChecked,tunnelsLeft,LEFT);
-      break;
-  }
+  makeSingleRoad(xNotChecked,yNotChecked,tunnelsLeft,direction);
+  // switch (whichDirectionNext) {
+  //   case DOWN:
+  //     break;
+  //   case RIGHT:
+  //     makeSingleRoad(xNotChecked,yNotChecked,tunnelsLeft,RIGHT);
+  //     break;
+  //   case UP:
+  //     makeSingleRoad(xNotChecked,yNotChecked,tunnelsLeft,UP);
+  //     break;
+  //   case LEFT:
+  //     makeSingleRoad(xNotChecked,yNotChecked,tunnelsLeft,LEFT);
+  //     break;
+  // }
   return;
 }
 
@@ -94,29 +94,38 @@ void Generator::makeSingleTunnel(int &xNotChecked,int &yNotChecked,
     return;
   }
   --stepsLeft;
-  if( xNotChecked<0 || xNotChecked>=size || yNotChecked<0 || yNotChecked>=size){
-    return;
-  }
   map->board[yNotChecked][xNotChecked] = 'D';
-
   switch (direction) {
     case DOWN:
-      --yNotChecked;
-      makeSingleTunnel(xNotChecked,yNotChecked,stepsLeft,DOWN);
-      break;
+    --yNotChecked;
+    break;
     case RIGHT:
-      ++xNotChecked;
-      makeSingleTunnel(xNotChecked,yNotChecked,stepsLeft,RIGHT);
-      break;
+    ++xNotChecked;
+    break;
     case UP:
-      ++yNotChecked;
-      makeSingleTunnel(xNotChecked,yNotChecked,stepsLeft,UP);
-      break;
+    ++yNotChecked;
+    break;
     case LEFT:
-      --xNotChecked;
-      makeSingleTunnel(xNotChecked,yNotChecked,stepsLeft,LEFT);
-      break;
+    --xNotChecked;
+    break;
   }
+  while( xNotChecked<0 || xNotChecked>=size || yNotChecked<0 || yNotChecked>=size){
+    switch (direction) {
+      case DOWN:
+      --yNotChecked;
+      break;
+      case RIGHT:
+      ++xNotChecked;
+      break;
+      case UP:
+      ++yNotChecked;
+      break;
+      case LEFT:
+      --xNotChecked;
+      break;
+    }
+  }
+  makeSingleTunnel(xNotChecked,yNotChecked,stepsLeft,direction);
   return;
 }
 
